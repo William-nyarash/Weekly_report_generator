980df12d70971031d03e8152e1c1eaa4fc6d1f0f
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-07-10 09:01:24 +0300

    initial commit

--- server/controllers/attendance.controller.js
+++ server/controllers/attendance.controller.js
@@ -1,11 +1,62 @@
-const express = require("express")
-const router = express.Router()
+const attendanceService = require("../services/attendance.service");
 
-const attendanceController = require('../controllers/attendance.controller')
+const createWeekAttendance = async (req, res, next) => {
+    try {
 
-router.route('/')
-.get(attendanceController.getWeekAttendace)
-.post(attendanceController.createWeekAttendance)
-.put(attendanceController.updateWeekAttendance)
+        const attendance = await attendanceService.createWeekAttendance(
+            req.params.reportId,
+            req.body.attendance
+        );
 
-module.exports = router
+        res.status(201).json({
+            status: "success",
+            results: attendance.length,
+            data: attendance
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getWeekAttendance = async (req, res, next) => {
+    try {
+
+        const attendance = await attendanceService.getWeekAttendance(
+            req.params.reportId
+        );
+
+        res.status(200).json({
+            status: "success",
+            results: attendance.length,
+            data: attendance
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+const updateWeekAttendance = async (req, res, next) => {
+    try {
+
+        const attendance = await attendanceService.updateWeekAttendance(
+            req.params.reportId,
+            req.body.attendance
+        );
+
+        res.status(200).json({
+            status: "success",
+            data: attendance
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {
+    createWeekAttendance,
+    getWeekAttendance,
+    updateWeekAttendance
+};
