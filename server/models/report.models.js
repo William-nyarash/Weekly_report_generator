d81007e4650a0c1a74cfbef43e3fc80adf2e6816
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-07-12 17:30:02 +0300

    feat: add update report function

--- server/models/report.models.js
+++ server/models/report.models.js
@@ -54,9 +54,53 @@ const createReport = async (data) => {
     }
 }
 
+const updateReport = async (id, data) => {
+    console.log("the parameter ", id)
+    const {
+        teacher_id,
+        week_start,
+        week_end,
+        tutors_class_attendance,
+        new_admissions_and_dropouts,
+        weeks_activity_completion,
+        weeks_activity_not_completed,
+        aobs,
+        assessment,
+        remarks
+    } = data
+    const sqlQuery = ` 
+    UPDATE reports SET teacher_id = $1,
+    week_start = $2,
+    week_end = $3,
+    tutors_class_attendance = $4,
+    new_admissions_and_dropouts = $5,
+    weeks_activity_completion = $6,
+    weeks_activity_not_completed = $7,
+    aobs = $8,
+    assessment = $9,
+    remarks = $10
+    WHERE id= $11
+    RETURNING *`
 
+    const result = await pool.query(sqlQuery,[
+        teacher_id,
+        week_start,
+        week_end,
+        tutors_class_attendance,
+        new_admissions_and_dropouts,
+        weeks_activity_completion,
+        weeks_activity_not_completed,
+        aobs,
+        assessment,
+        remarks
+    ])
+
+    return result.rows[0]
+
+} 
 module.exports = {
     getAllReports,
+    updateReport,
     createReport,
     getReport,   
 }
