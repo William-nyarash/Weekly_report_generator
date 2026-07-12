7615fa9df944b597813ffa234250c135a85d09de
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-07-12 17:26:08 +0300

    fix: fix the nested route error, refactor the route

--- server/routes/attendance.routes.js
+++ server/routes/attendance.routes.js
@@ -1,10 +1,10 @@
 const express = require("express")
-const router = express.Router()
+const router = express.Router({ mergeParams: true})
 
 const attendanceController = require('../controllers/attendance.controller')
 
 router.route('/')
-.get(attendanceController.getWeekAttendace)
+.get(attendanceController.getWeekAttendance)
 .post(attendanceController.createWeekAttendance)
 .put(attendanceController.updateWeekAttendance)
 
