dc4f438fb16d104feae05b0055fe86b6c68b6665
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-07-29 11:47:29 +0300

    fix creating attendance

--- server/services/attendance.service.js
+++ server/services/attendance.service.js
@@ -5,11 +5,16 @@ const DAYS = [
     "Tuesday",
     "Wednesday",
     "Thursday",
-    "Friday"
+    "Friday",
 ];
 
 const createWeekAttendance = async (reportId, attendance) => {
-    
+    const existingAttendance = await Attendance.findIfExists(reportId);
+
+    if (existingAttendance.length > 0) {
+        throw new Error("Attendance already exists.");
+    }
+
     if (!Array.isArray(attendance)) {
         throw new Error("Attendance must be an array.");
     }
@@ -18,7 +23,7 @@ const createWeekAttendance = async (reportId, attendance) => {
         throw new Error("Exactly five attendance records are required.");
     }
 
-    const days = attendance.map(day => day.day_name);
+    const days = attendance.map((item) => item.day_name);
 
     for (const day of DAYS) {
         if (!days.includes(day)) {
@@ -26,7 +31,15 @@ const createWeekAttendance = async (reportId, attendance) => {
         }
     }
 
-    return await Attendance.createWeekAttendance(reportId, attendance);
+    const attendanceData = attendance.map((item) => ({
+        report_id: reportId,
+        day_name: item.day_name,
+        present_students: item.present_students,
+        absent_students: item.absent_students,
+        teacher_comment: item.teacher_comment || "",
+    }));
+
+    return await Attendance.createWeekAttendance(attendanceData);
 };
 
 const getWeekAttendance = async (reportId) => {
@@ -40,5 +53,5 @@ const updateWeekAttendance = async (reportId, attendance) => {
 module.exports = {
     createWeekAttendance,
     getWeekAttendance,
-    updateWeekAttendance
+    updateWeekAttendance,
 };
