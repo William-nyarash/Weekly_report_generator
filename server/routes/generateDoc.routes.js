987d9604a55bf27bdc9352b8078c6c6a7b33ee6f
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-07-30 23:09:46 +0300

    add pdf route

--- server/routes/generateDoc.routes.js
+++ server/routes/generateDoc.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router({mergeParams: true})
-const generateDocument = require("../controllers/reportGenerator.controller")
+const { downloadReport, generateDocument} = require("../controllers/reportGenerator.controller")
 
 router.get('/generate', generateDocument)
+router.get('/pdf', downloadReport)
 
 module.exports = router
