52b6884c9ddfda7aaf0cface4af31db170f60946
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-05 15:01:26 +0300

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+
+    server: {
+      proxy: {
+        "/gti": {
+          target: env.PROD_SERVER,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/gti/, ""),
+        },
+      },
+    },
+  };
+});
