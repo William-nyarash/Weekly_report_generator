7deb6bfc489df4c5e07d6136943938335058d675
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-06 15:14:44 +0300

    refactor code

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "/gti"
-    : import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL
 });
 
 export default api;
