d0740c2f9518d5e75ff82d61dbd1f72f90d17254
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-20 14:06:19 +0300

    update style

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -4,12 +4,12 @@ import logo from "../assets/logo.jpg"
 export default function Layout() {
   return (
     <div className="min-h-screen bg-[rgba(148,131,131,0.24)]">
-      <header className="bg-orange-500 flex flex-row gap-5 justify-content-between items-center text-white text-3xl font-bold p-5 shadow-md">
-        <div className="h-6 w-6 md:w-12 md:h-12 overflow-hidden">
+      <header className="bg-orange-500 flex flex-row items-center justify-between md:justify-evenly gap-5 text-white text-3xl font-bold p-5 shadow-md">
+        <div className="h-12 w-12 md:w-12 md:h-12 overflow-hidden">
           <img src={logo} alt="gorgeous institute" className="w-full h-auto object-cover" />
         </div>
-        <div className="text-sm   flex flex-col items-center">
-          <div className="md:text-3xl">Gorgeous technical  institute</div>
+        <div className="flex flex-col items-center">
+          <div className="text-sm md:text-3xl">Gorgeous technical  institute</div>
           <div className="self-center  md:text-xl text-sm font-light">weekly report</div> 
         </div>
       </header>
